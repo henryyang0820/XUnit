ce72478796372a6cc2f13a36c60ee09d1551294d
henryyang0820 <1045033116@qq.com>
2019-10-26 22:02:26 +0800

    init

---
+++ src/test/java/TestJUnit4Demo.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestJUnit4Demo {
+    @Test
+    public void testDemo(){
+        assertTrue(true);
+
+    }
+    @Test
+    public void testDemo2(){
+        assertTrue(false);
+    }
+}
+    git config --global user.email "1045033116@qq.com"
+        git config --global user.name "henryyang0820"
