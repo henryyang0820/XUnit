3b35a69eed0dbf3bb7d2fc2ceacecd8632972e8f
“henryyang0820git config --global user.email “y1045033116@qq.com <1045033116@qq.com>
2019-10-31 15:34:33 +0800

    重构

--- src/test/java/selenium/pages/App.java
+++ src/test/java/selenium/pages/App.java
@@ -1,37 +1,36 @@
 package selenium.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.By.linkText;
 
-public class App extends BasePage{
+public class App extends BasePage {
 
-    public App loginWithCookie(){
-        String url =  "https://work.weixin.qq.com/";
+    public App loginWithCookie() {
+        String url = "https://work.weixin.qq.com/";
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(url);
-        driver.manage().window().setSize(new Dimension(1406,877));
+        driver.manage().window().setSize(new Dimension(1406, 877));
         driver.findElement(linkText("企业登录")).click();
 
         System.out.println(driver.manage().getCookies());
 
-        driver.manage().addCookie(new Cookie("wwrtx.refid","42351602663264511"));
-        driver.manage().addCookie(new Cookie("wwrtx.sid","8DFD-5olxbVx4RGaioEu3QDYSeFRYj-sSDJtIMnfujqu-vgIfBhM5cNjXE_sCGLU"));
+        driver.manage().addCookie(new Cookie("wwrtx.refid", "42351602663264511"));
+        driver.manage().addCookie(new Cookie("wwrtx.sid", "8DFD-5olxbVx4RGaioEu3QDYSeFRYj-sSDJtIMnfujqu-vgIfBhM5cNjXE_sCGLU"));
         driver.navigate().refresh();
         return this;
     }
 
-    public ConTactPage toContact(){
+    public ConTactPage toContact() {
         return new ConTactPage();
     }
-    public ConTactPage toAddMember(){
+
+    public ConTactPage toAddMember() {
         findElement(linkText("添加成员")).click();
         return new ConTactPage();
     }
