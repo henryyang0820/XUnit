c1e632218461d8c8632fe033c6e4e309a71a9d8a
“henryyang0820git config --global user.email “y1045033116@qq.com <1045033116@qq.com>
2019-11-04 16:22:46 +0800

    preparation of homework

--- src/test/java/selenium/pages/BasePage.java
+++ src/test/java/selenium/pages/BasePage.java
@@ -3,11 +3,35 @@ package selenium.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
-    public static WebDriver driver;
+  public static RemoteWebDriver driver;
 
     public WebElement findElement(By by){
+        return findElement(by, 5);
+    }
+    public WebElement findElement(By by, int timeout){
+        System.out.println(by);
+        if(timeout>0) {
+            waitClickable(by, timeout);
+            System.out.println("clickable");
+        }
         return driver.findElement(by);
+    }
+    public void waitClickable(By by, int timeout){
+        new WebDriverWait(driver, timeout).until(ExpectedConditions.elementToBeClickable(by));
+    }
+    public void waitClickable(By by){
+        new WebDriverWait(driver, 3).until(ExpectedConditions.visibilityOfElementLocated(by));
+        new WebDriverWait(driver, 3).until(ExpectedConditions.elementToBeClickable(by));
+
+    }
+
+    public void quit() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
