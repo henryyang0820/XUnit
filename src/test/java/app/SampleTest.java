81594d88a1920c5186480e8bda5c07f6c25de3c1
“henryyang0820git config --global user.email “y1045033116@qq.com <1045033116@qq.com>
2019-11-12 14:05:55 +0800

    ignore

--- src/test/java/app/SampleTest.java
+++ src/test/java/app/SampleTest.java
@@ -35,8 +35,8 @@ public class SampleTest {
         MobileElement el1 = (MobileElement) driver.findElementById("com.xueqiu.android:id/home_search");
         el1.click();
         MobileElement el2 = (MobileElement) driver.findElementById("com.xueqiu.android:id/search_input_text");
-        el2.sendKeys("alibaba");
-        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.TextView[1]");
+        el2.sendKeys("ningdeshidai");
+        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]");
         el3.click();
         MobileElement el4 = (MobileElement) driver.findElementById("com.xueqiu.android:id/current_price");
         el4.click();
